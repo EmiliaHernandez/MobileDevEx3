c2c36c7daddd00165ee37ffadc00fdfeb70c0e60
Emilia Hernandez <emilia.b.hernandez@gmail.com>
2024-02-05 21:21:20 -0500

    3.3 and 3.4 complete

--- exercises.js
+++ exercises.js
@@ -1,4 +1,3 @@
-
 //3-1 flattening
 let arrays = [[1, 2, 3], [4, 5], [6]];
 var merged = [].concat.apply([], arrays);
@@ -17,3 +16,24 @@ function loop(value, test, update, body) {
     (n) => n - 1,
     console.log
   );
+
+//3.3 3.4
+export function everyLoop(array, test) 
+{
+  let curResult = true;
+  for(let item of array){
+    curResult = curResult && test(item)
+  } return curResult
+}
+
+export function everySome(array, test) 
+{
+return !array.some((value)=>!test(value))
+} 
+
+console.log(everyLoop([1, 3, 5], n => n < 10));
+// → true
+console.log(everyLoop([2, 4, 16], n => n < 10));
+// → false
+console.log(everyLoop([], n => n < 10));
+// → true
